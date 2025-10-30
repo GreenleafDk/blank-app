13f6839c6e697fdee77e9c5a67b3d653cd0d233b
GreenleafDk <100238021+GreenleafDk@users.noreply.github.com>
2025-10-30 17:37:11 +0000

    mere

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,2 @@
-
-import streamlit as st
-import pydeck as pdk
-
-
-
-#pdk.Deck(api_keys={"mapbox": "pk.eyJ1IjoidGhvbWFzZ3JlZW5sZWFmIiwiYSI6ImNtaGRvanNrbTA0b3Mya3M4cTJ0MGs0eDYifQ.tvchESFaPyVz_v6dSSG0jQ"})
-
-st.login()
+import Authlib
+st.write("Authlib version:", Authlib.__version__)
