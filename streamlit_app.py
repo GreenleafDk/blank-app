ec2744a4655cfc839185648a8861b7c4c0885126
GreenleafDk <100238021+GreenleafDk@users.noreply.github.com>
2025-10-30 17:32:02 +0000

    first

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
-import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+import streamlit as st
+import pydeck as pdk
+
+
+
+#pdk.Deck(api_keys={"mapbox": "pk.eyJ1IjoidGhvbWFzZ3JlZW5sZWFmIiwiYSI6ImNtaGRvanNrbTA0b3Mya3M4cTJ0MGs0eDYifQ.tvchESFaPyVz_v6dSSG0jQ"})
+
+st.login()
